86bc32a9e243da3238d0697891f8095b81f24c01
David Culver <culver.david.p@gmail.com>
2021-07-05 20:34:22 -0500

    modified color exercise

--- src/main/java/viewColorServlet.java
+++ src/main/java/viewColorServlet.java
@@ -11,8 +11,8 @@ public class viewColorServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String newcolor = req.getQueryString();
-        System.out.println(newcolor);
+//        String newcolor = req.getQueryString();
+//        System.out.println(newcolor);
         String colorchoice = req.getParameter("colorchoice");
         req.setAttribute("colorchoice", colorchoice);
         req.getRequestDispatcher("/viewColor.jsp").forward(req, resp);
