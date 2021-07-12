add7a594a2f4fcf9fdc006db911350a0e3a22464
David Culver <culver.david.p@gmail.com>
2021-07-12 10:59:15 -0500

    user input lecture

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -38,9 +38,14 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public Long insert(Ad ad) {
+        String sql = "INSERT INTO ads(title, description, user_id) VALUES (?, ?, ?);";
         try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+//            returngenkeys allows us to see ad after its created
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, ad.getTitle());
+            stmt.setString(2, ad.getDescription());
+            stmt.setLong(3, ad.getUserId());
+            stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getLong(1);
@@ -49,17 +54,17 @@ public class MySQLAdsDao implements Ads {
         }
     }
 
-    @Override
-    public Ad getOne(long id){
-        try {
-            Statement stmt = connection.createStatement();
-            stmt.executeQuery("SELECT FROM ads WHERE id = " + id);
-            ResultSet rs = stmt.getGeneratedKeys();
-            return extractAd(rs);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error creating a new ad.", e);
-        }
-    }
+//    @Override
+//    public Ad getOne(long id){
+//        try {
+//            Statement stmt = connection.createStatement();
+//            stmt.executeQuery("SELECT FROM ads WHERE id = " + id);
+//            ResultSet rs = stmt.getGeneratedKeys();
+//            return extractAd(rs);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error creating a new ad.", e);
+//        }
+//    }
 
     private String createInsertQuery(Ad ad) {
         return "INSERT INTO ads(user_id, title, description) VALUES "
@@ -83,5 +88,24 @@ public class MySQLAdsDao implements Ads {
             ads.add(extractAd(rs));
         }
         return ads;
+    }
+
+    @Override
+    public List<Ad> searchByTitle(String query) {
+//        String sql = "SELECT * FROM ads WHERE title '%" + query + "%';";
+//        Change to use a prepared statement with ?
+        String sql = "SELECT * FROM ads WHERE title LIKE ?;";
+        String searchTermWithWildCards = "%" + query + "%";
+        //        Instead of using plain statement - use prepared statement to be able to use an apostrophe in search/add
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement(sql);
+//            Inside a prepared statement, begins with index of 1. Setting the value of the string that was prepared in the preparedstatement
+            stmt.setString(1, searchTermWithWildCards);
+            ResultSet rs = stmt.executeQuery();
+            return createAdsFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
     }
 }
